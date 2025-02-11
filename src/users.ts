d157ae9d55903977d4acde31c40cf36c872527b9
Caleb Norton <n0603919@outlook.com>
2025-02-11 01:32:45 -0600

    test email routing

--- src/users.ts
+++ src/users.ts
@@ -1,5 +1,6 @@
 const mappings = [
     { "email": "webmaster@ieeetamu.org", "recipient": "chnorton@tamu.edu" },
+    { "email": "test@ieeetamu.org", "recipient": "n0603919@outlook.com" },
 ]
 
 export default mappings;
