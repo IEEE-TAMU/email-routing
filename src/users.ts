41c6f0075841f7fe61226a8941a72df77ca4973b
Caleb Norton <n0603919@outlook.com>
2025-02-11 01:35:50 -0600

    test works

--- src/users.ts
+++ src/users.ts
@@ -1,6 +1,5 @@
 const mappings = [
     { "email": "webmaster@ieeetamu.org", "recipient": "chnorton@tamu.edu" },
-    { "email": "test@ieeetamu.org", "recipient": "n0603919@outlook.com" },
 ]
 
 export default mappings;
