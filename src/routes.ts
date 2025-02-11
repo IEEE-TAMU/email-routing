31d51e38013676b808306d7a61a6b1c8b504308c
Caleb Norton <n0603919@outlook.com>
2025-02-11 15:41:10 -0600

    add pi

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 const Routes = [
     { "destination": "webmaster@ieeetamu.org", "recipients": ["chnorton@tamu.edu"] },
     { "destination": "marketing@ieeetamu.org", "recipients": ["chnorton@tamu.edu", "n0603919@outlook.com"] },
+    { "destination": "pi@ieeetamu.org", "recipients": ["mbfritzer@tamu.edu"] },
 ]
 
 const DefaultRecipient = "chnorton@tamu.edu";
