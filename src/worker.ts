c91750d45f318a016e647db0c8f0279845e452d3
Caleb Norton <n0603919@outlook.com>
2025-02-11 01:22:54 -0600

    Add catch-all

--- src/worker.ts
+++ src/worker.ts
@@ -1,12 +1,14 @@
 import mappings from "./users";
 
+const default_email = "chnorton@tamu.edu";
+
 export default {
   async email(message, env, ctx) {
     const recipient = mappings.find(({ email }) => email === message.to)?.recipient;
     if (recipient) {
       await message.forward(recipient);
     } else {
-      message.setReject("Unknown address");
+      await message.forward(default_email);
     }
   }
 }
