e1913c2cd8b668e5965eb73849ac34e4371b77bc
Caleb Norton <n0603919@outlook.com>
2025-02-11 11:01:27 -0600

    better logging when no route available

--- src/worker.ts
+++ src/worker.ts
@@ -2,7 +2,11 @@ import { Routes, DefaultRecipient } from "./routes";
 
 export default {
   async email(message: ForwardableEmailMessage, env: unknown, ctx: ExecutionContext) {
-    const recipient = Routes.find(({ destination }) => destination === message.to)?.recipient ?? DefaultRecipient;
+    let recipient = Routes.find(({ destination }) => destination === message.to)?.recipient
+    if (!recipient) {
+      console.log(`No route found for email to ${message.to}, forwarding to default recipient: ${DefaultRecipient}`);
+      recipient = DefaultRecipient;
+    }
     console.log(`Forwarding email from ${message.from} to ${recipient}`);
     try {
       await message.forward(recipient);
