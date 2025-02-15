f2d3b6a71ccf67846c31ed606c28e449b3b1dc9e
Caleb Norton <n0603919@outlook.com>
2025-02-15 17:13:11 -0600

    simplify multi-forwarding

--- src/worker.ts
+++ src/worker.ts
@@ -3,27 +3,7 @@ import { Routes, DefaultRecipient } from "./routes";
 
 // Execution Environment as defined in the wranger.toml file
 // See cloudflare docs for information on how to define this
-interface Environment {
-  MAIL: {
-    send(message: EmailMessage): Promise<void>
-  }
-}
-
-// format a banner to remind users to CC other officers that are on the route
-function makeBanner(otherRecipients: string[]) {
-  // TODO: make banner better
-  return "This email was sent to multiple officers. Please remember to CC all officers who got this message. \n\n" + otherRecipients.join(", ");
-}
-
-async function messageWithBanner(message: ForwardableEmailMessage, recipient: string, otherRecipients: string[]) {
-  const banner = makeBanner(otherRecipients);
-  const body = await new Response(message.raw).text();
-  console.log(`banner: ${banner}`);
-  console.log(`body: ${body}`);
-  const newBody = banner + "\n\n" + body;
-
-  return new EmailMessage(message.from, recipient, newBody);
-}
+interface Environment {}
 
 
 async function forwardToSingleRecipient(message: ForwardableEmailMessage, recipient: string) {
@@ -36,12 +16,7 @@ async function forwardToSingleRecipient(message: ForwardableEmailMessage, recipi
 
 async function forwardToMultipleRecipients(message: ForwardableEmailMessage, recipients: string[], env: Environment) {
   for (const recipient of recipients) {
-    try {
-      const newMessage = await messageWithBanner(message, recipient, recipients.filter(r => r !== recipient));
-      await env.MAIL.send(newMessage);
-    } catch (e) {
-      console.error(`Failed to forward email to ${recipient}: ${e}`);
-    }
+    forwardToSingleRecipient(message, recipient);
   }
 }
 
