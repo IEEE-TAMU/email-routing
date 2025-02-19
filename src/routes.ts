d0e2e70fab8d7ad05ea410e8d34c13a5cab45abc
Caleb Norton <n0603919@outlook.com>
2025-02-19 15:24:36 -0600

    remove pi and add noreply

--- src/routes.ts
+++ src/routes.ts
@@ -5,8 +5,8 @@ const BlackholeRecipient = "blackhole";
 const Routes = [
     { "destination": "webmaster@ieeetamu.org", "recipients": ["chnorton@tamu.edu"] },
     { "destination": "marketing@ieeetamu.org", "recipients": ["chnorton@tamu.edu", "n0603919@outlook.com"] },
-    { "destination": "pi@ieeetamu.org", "recipients": ["mbfritzer@tamu.edu"] },
     { "destination": "donotreply@ieeetamu.org", "recipients": [BlackholeRecipient] },
+    { "destination": "noreply@ieeetamu.org", "recipients": [BlackholeRecipient] },
 ]
 
-export { Routes, DefaultRecipient , BlackholeRecipient };
+export { Routes, DefaultRecipient, BlackholeRecipient };
