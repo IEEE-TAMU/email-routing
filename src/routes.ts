50f30ce88fd51d3aa0a0ed0e0806add208f7076d
Caleb Norton <n0603919@outlook.com>
2025-04-14 12:01:05 -0500

    update emails

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,9 @@
-const DefaultRecipient = "chnorton@tamu.edu";
+const DefaultRecipient = "chnorton+ieee@tamu.edu";
 
 const BlackholeRecipient = "blackhole";
 
 const Routes = [
-    { "destination": "webmaster@ieeetamu.org", "recipients": ["chnorton@tamu.edu"] },
-    { "destination": "marketing@ieeetamu.org", "recipients": ["chnorton@tamu.edu", "n0603919@outlook.com"] },
+    { "destination": "webmaster@ieeetamu.org", "recipients": ["chnorton+ieee@tamu.edu"] },
     { "destination": "president@ieeetamu.org", "recipients": ["oliver.jansen@tamu.edu"] },
     { "destination": "donotreply@ieeetamu.org", "recipients": [BlackholeRecipient] },
     { "destination": "noreply@ieeetamu.org", "recipients": [BlackholeRecipient] },
