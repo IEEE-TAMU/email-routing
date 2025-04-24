c53ad3a02af660e948f912ca30c92c6fbc3e7de2
Caleb Norton <n0603919@outlook.com>
2025-04-24 13:44:26 -0500

    Add alan

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ const Routes = [
     { "destination": "webmaster@ieeetamu.org", "recipients": ["chnorton@tamu.edu"] },
     { "destination": "president@ieeetamu.org", "recipients": ["oliver.jansen@tamu.edu"] },
     { "destination": "vicepresident@ieeetamu.org", "recipients": ["smayhue@tamu.edu"] },
+    { "destination": "events@ieeetamu.org", "recipients": ["alanjaf@tamu.edu"] },
     { "destination": "donotreply@ieeetamu.org", "recipients": [BlackholeRecipient] },
     { "destination": "noreply@ieeetamu.org", "recipients": [BlackholeRecipient] },
 ]
