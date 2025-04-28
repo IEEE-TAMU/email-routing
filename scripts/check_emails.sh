d5e7a5ed251cfb0ab598a6186f12fdf6a504dcce
Caleb Norton <n0603919@outlook.com>
2025-04-28 13:26:32 -0500

    new officers!

--- scripts/check_emails.sh
+++ scripts/check_emails.sh
@@ -13,20 +13,25 @@ echo
 # get emails from the routes.ts file
 emails_in_routes=$(grep -Eo '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}' src/routes.ts | grep -v ieeetamu.org|sort -u)
 
-echo "Emails in routes.ts:"
-echo "$emails_in_routes"
-echo
+# echo "Emails in routes.ts:"
+# echo "$emails_in_routes"
+# echo
 
 has_unverified=false
 # check if the emails in the routes.ts file are verified
 for email in $emails_in_routes; do
   if ! echo "$emails" | grep -q "$email"; then
-    echo "Unverified email: $email"
+    if [ "$has_unverified" = false ]; then
+      echo "Unverified emails:"
+      has_unverified=true
+    fi
+    echo "$email"
     has_unverified=true
   fi
 done
 
 if [ "$has_unverified" = true ]; then
+  echo
   echo "There are unverified emails in the routes.ts file."
   exit 1
 else
--- src/routes.ts
+++ src/routes.ts
@@ -3,10 +3,20 @@ const DefaultRecipient = "chnorton@tamu.edu";
 const BlackholeRecipient = "blackhole";
 
 const Routes = [
-    { "destination": "webmaster@ieeetamu.org", "recipients": ["chnorton@tamu.edu"] },
     { "destination": "president@ieeetamu.org", "recipients": ["oliver.jansen@tamu.edu"] },
     { "destination": "vicepresident@ieeetamu.org", "recipients": ["smayhue@tamu.edu"] },
+    { "destination": "treasurer@ieeetamu.org", "recipients": ["nafibaksh@tamu.edu"] },
+    { "destination": "secretary@ieeetamu.org", "recipients": ["akafle@tamu.edu"] },
+
+    { "destination": "webmaster@ieeetamu.org", "recipients": ["chnorton@tamu.edu"] },
+    { "destination": "historian@ieeetamu.org", "recipients": ["gjohny@tamu.edu"] },
+
     { "destination": "events@ieeetamu.org", "recipients": ["alanjaf@tamu.edu"] },
+    { "destination": "tec@ieeetamu.org", "recipients": ["jyotiverma607@tamu.edu"] },
+    { "destination": "publicrelations@ieeetamu.org", "recipients": ["liannie@tamu.edu"] },
+    { "destination": "corporate@ieeetamu.org", "recipients": ["amk23@tamu.edu"] },
+    // { "destination": "activities@ieeetamu.org", "recipients": ["TBD"]},
+
     { "destination": "donotreply@ieeetamu.org", "recipients": [BlackholeRecipient] },
     { "destination": "noreply@ieeetamu.org", "recipients": [BlackholeRecipient] },
 ]
