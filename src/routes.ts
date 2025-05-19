3941548f48de69cbbef5465a6d0fe08935027db9
Caleb Norton <n0603919@outlook.com>
2025-05-19 13:47:16 -0500

    use gmail inboxes

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,7 @@ const BlackholeRecipient = "blackhole";
 const Routes = [
     { "destination": "president@ieeetamu.org", "recipients": ["oliver.jansen@tamu.edu"] },
     { "destination": "vicepresident@ieeetamu.org", "recipients": ["smayhue@tamu.edu"] },
-    { "destination": "treasurer@ieeetamu.org", "recipients": ["nafibaksh@tamu.edu"] },
+    { "destination": "treasurer@ieeetamu.org", "recipients": [ "ieeetamu.cfo@gmail.com " ] }, //"nafibaksh@tamu.edu"] },
     { "destination": "secretary@ieeetamu.org", "recipients": ["akafle@tamu.edu"] },
 
     { "destination": "webmaster@ieeetamu.org", "recipients": ["chnorton@tamu.edu"] },
@@ -14,7 +14,7 @@ const Routes = [
     { "destination": "events@ieeetamu.org", "recipients": ["alanjaf@tamu.edu"] },
     { "destination": "tec@ieeetamu.org", "recipients": ["jyotiverma607@tamu.edu"] },
     { "destination": "publicrelations@ieeetamu.org", "recipients": ["liannie@tamu.edu"] },
-    { "destination": "corporate@ieeetamu.org", "recipients": ["amk23@tamu.edu"] },
+    { "destination": "corporate@ieeetamu.org", "recipients": ["ieeetamu.corporate@gmail.com" ] },
     // { "destination": "activities@ieeetamu.org", "recipients": ["TBD"]},
 
     { "destination": "donotreply@ieeetamu.org", "recipients": [BlackholeRecipient] },
